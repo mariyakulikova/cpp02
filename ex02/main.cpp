08724359f844ead3fdedbd5bb004467b061abddf
mkulikov <mkulikov@student.42berlin.de>
2025-03-04 21:55:50 +0100

    start ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkulikov <mkulikov@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 11:50:40 by mkulikov          #+#    #+#             */
+/*   Updated: 2025/03/04 21:53:13 by mkulikov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+ostream &operator<<(ostream& os, const Fixed& fixed) {
+	os << fixed.toFloat();
+	return os;
+}
+
+int main()
+{
+	Fixed a;
+	Fixed const b(10);
+	Fixed const c(42.42f);
+	Fixed const d(b);
+	a = Fixed(1234.4321f);
+
+	return 0;
+}
