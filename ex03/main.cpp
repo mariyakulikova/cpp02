52f73b4d6bcfac1ae2fc4df44116f2ad9aa27259
mkulikov <mkulikov@student.42berlin.de>
2025-03-09 11:40:50 +0100

    add test case

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mkulikov <mkulikov@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 11:50:40 by mkulikov          #+#    #+#             */
-/*   Updated: 2025/03/09 11:29:53 by mkulikov         ###   ########.fr       */
+/*   Updated: 2025/03/09 11:40:20 by mkulikov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,13 +46,14 @@ int main() {
 	test(d, f, g, Point(6, 6), false, "Test 4: Point Outside");
 	test(d, f, g, Point(3, 3), true, "Test 5: Point Inside");
 	test(d, f, g, Point(-4, -1), false, "Test 6: Point Outside");
+	test(d, f, g, Point(4, 4), true, "Test 7 Point Outside");
 
 	Point q(-4, 1);
 	Point w(2, 2);
 	Point e(3, -4);
-	test(q, w, e, Point(0, 0), true, "Test 7: Point Outside");
-	test(q, w, e, Point(3, 3), false, "Test 8: Point Inside");
-	test(q, w, e, Point(-3, 1), true, "Test 9: Point Outside");
+	test(q, w, e, Point(0, 0), true, "Test 8: Point Outside");
+	test(q, w, e, Point(3, 3), false, "Test 9: Point Inside");
+	test(q, w, e, Point(-3, 1), true, "Test 10: Point Outside");
 
 	return 0;
 }
