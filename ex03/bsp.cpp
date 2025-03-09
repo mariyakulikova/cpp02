71d60332242f67b6b3d3d742642fd47408edc3c5
mkulikov <mkulikov@student.42berlin.de>
2025-03-09 11:32:13 +0100

    return abs value of areaof triangle

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: mkulikov <mkulikov@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 11:46:48 by mkulikov          #+#    #+#             */
-/*   Updated: 2025/03/07 15:26:21 by mkulikov         ###   ########.fr       */
+/*   Updated: 2025/03/09 11:31:06 by mkulikov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ static Fixed area(Point const a, Point const b, Point const c) {
 	Fixed area = (a.getX().toFloat() * (b.getY().toFloat() - c.getY().toFloat()) +
 					b.getX().toFloat() * (c.getY().toFloat() - a.getY().toFloat()) +
 					c.getX().toFloat() * (a.getY().toFloat() - b.getY().toFloat())) / 2.0f;
-	// if (area < 0)
-	// {
-	// 	area = area * -1;
-	// }
+	if (area < 0)
+	{
+		area = area * -1;
+	}
 	return area;
 }
 
@@ -28,11 +28,6 @@ bool bsp(Point const a, Point const b, Point const c, Point const point) {
 	Fixed area1 = area(point, b, c);
 	Fixed area2 = area(a, point, c);
 	Fixed area3 = area(a, b, point);
-
-	if (area1 < 0 || area2 < 0 || area3 < 0)
-	{
-		return false;
-	}
 
 	return (fullArea == (area1 + area2 + area3));
 }
